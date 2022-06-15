31b81ff109df499d6dbdf825d8e86199813f29b3
KevinMathewT <mathewtkevin@gmail.com>
2022-06-15 17:01:41 +0530

    added changes

--- setup/jarvis.sh
+++ setup/jarvis.sh
@@ -1,12 +1,20 @@
 pwd
-mkdir /home/input/
-mkdir /home/generated/
-mkdir /home/weights/
-cd /home/input/
-kaggle competitions download -c feedback-prize-effectiveness
 sudo apt-get install unzip
+rm -rf /home/working/
+mkdir /home/working/input/
+mkdir /home/working/generated/
+mkdir /home/working/weights/
+
+cd /home/working/
+git init
+git pull https://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@github.com/KevinMathewT/FeedbackEPA-Kaggle
+cp /home/working/setup/kaggle.json /home/.kaggle/kaggle.json
+
+cd /home/working/input/
+kaggle competitions download -c feedback-prize-effectiveness
 unzip feedback-prize-effectiveness.zip -d .
-cd /home/
+
+cd /home/working/
 git init
 git pull https://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@github.com/KevinMathewT/FeedbackEPA-Kaggle
 python3 -m src.create_folds
