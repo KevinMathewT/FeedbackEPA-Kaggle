bc64323b4885808776a2f9232f0f31f11449c456
KevinMathewT <mathewtkevin@gmail.com>
2022-06-23 15:41:22 +0530

    some changes

--- src/train.py
+++ src/train.py
@@ -16,7 +16,7 @@ def train_one_epoch(
 
     dataset_size = 0
     running_loss = 0.0
-    running_bce_loss = 0.0
+    running_ce_loss = 0.0
     st = time()
 
     # bar = tqdm(enumerate(dataloader), total=len(dataloader))
@@ -31,7 +31,7 @@ def train_one_epoch(
 
         loss = criterion(outputs, targets)
         loss = loss / config["n_accumulate"]
-        bce_loss = nn.CrossEntropyLoss()(outputs, targets)
+        ce_loss = nn.CrossEntropyLoss()(outputs, targets)
         accelerator.backward(loss)
         wandb.log({"train step loss": loss})
 
@@ -45,14 +45,14 @@ def train_one_epoch(
                 scheduler.step()
 
         running_loss += loss.item() * batch_size
-        running_bce_loss += bce_loss.item() * batch_size
+        running_ce_loss += ce_loss.item() * batch_size
         dataset_size += batch_size
 
         epoch_loss = running_loss / dataset_size
-        epoch_bce_loss = running_bce_loss / dataset_size
+        epoch_ce_loss = running_ce_loss / dataset_size
 
         if step == 0 or (step + 1) % config['freq'] == 0 or step == len(dataloader) - 1:
-            accelerator.print(f"[{epoch}/{config['epochs']}][{str(step + 1):5s}/{len(dataloader)}] train loss: {epoch_loss:1.10f} | bce_loss: {epoch_bce_loss:1.10f} | lr: {optimizer.param_groups[0]['lr']:1.10f} | time: {time() - st:1.1f}s")
+            accelerator.print(f"[{epoch}/{config['epochs']}][{str(step + 1):5s}/{len(dataloader)}] train loss: {epoch_loss:1.10f} | ce_loss: {epoch_ce_loss:1.10f} | lr: {optimizer.param_groups[0]['lr']:1.10f} | time: {time() - st:1.1f}s")
         
         # optimizer.param_groups[0]["lr"]
         # bar.set_postfix(
@@ -69,7 +69,7 @@ def valid_one_epoch(model, dataloader, criterion, accelerator, epoch):
 
     dataset_size = 0
     running_loss = 0.0
-    running_bce_loss = 0.0
+    running_ce_loss = 0.0
     st = time()
 
     # bar = tqdm(enumerate(dataloader), total=len(dataloader))
@@ -83,18 +83,18 @@ def valid_one_epoch(model, dataloader, criterion, accelerator, epoch):
         outputs = model(ids, mask)
 
         loss = criterion(outputs, targets)
-        bce_loss = nn.CrossEntropyLoss()(outputs, targets)
+        ce_loss = nn.CrossEntropyLoss()(outputs, targets)
         wandb.log({"valid step loss": loss})
 
         running_loss += loss.item() * batch_size
-        running_bce_loss += bce_loss.item() * batch_size
+        running_ce_loss += ce_loss.item() * batch_size
         dataset_size += batch_size
 
         epoch_loss = running_loss / dataset_size
-        epoch_bce_loss = running_bce_loss / dataset_size
+        epoch_ce_loss = running_ce_loss / dataset_size
 
         if step == 0 or (step + 1) % config['freq'] == 0 or step == len(dataloader) - 1:
-            accelerator.print(f"[{epoch}/{config['epochs']}][{str(step + 1):5s}/{len(dataloader)}] valid loss: {epoch_loss:1.10f} | bce_loss: {epoch_bce_loss:1.10f} | time: {time() - st:1.1f}s")
+            accelerator.print(f"[{epoch}/{config['epochs']}][{str(step + 1):5s}/{len(dataloader)}] valid loss: {epoch_loss:1.10f} | ce_loss: {epoch_ce_loss:1.10f} | time: {time() - st:1.1f}s")
         # bar.set_postfix(Epoch=epoch, Valid_Loss=epoch_loss)
 
     gc.collect()
