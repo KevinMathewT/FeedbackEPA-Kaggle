5b6e2f85e1ce2ddb08f8c53090f228cf7be096c4
KevinMathewT <mathewtkevin@gmail.com>
2022-06-23 12:59:00 +0530

    some changes

--- src/model.py
+++ src/model.py
@@ -6,11 +6,34 @@ from transformers import AutoModel, AutoConfig
 from config import config
 
 
+class MultiDropout(nn.Module):
+    def __init__(self, model_config):
+        super(MultiDropout, self).__init__()
+        self.fc = nn.Linear(model_config.hidden_size, config["num_classes"])
+
+        # Multi Dropouts
+        self.dropout1 = nn.Dropout(0.1)
+        self.dropout2 = nn.Dropout(0.2)
+        self.dropout3 = nn.Dropout(0.3)
+        self.dropout4 = nn.Dropout(0.4)
+        self.dropout5 = nn.Dropout(0.5)
+
+    def forward(self, out):
+        logits1 = self.fc(self.dropout1(out))
+        logits2 = self.fc(self.dropout2(out))
+        logits3 = self.fc(self.dropout3(out))
+        logits4 = self.fc(self.dropout4(out))
+        logits5 = self.fc(self.dropout5(out))
+
+        out = (logits1 + logits2 + logits3 + logits4 + logits5) / 5
+
 class MeanPooling(nn.Module):
     def __init__(self, model_config):
         super(MeanPooling, self).__init__()
         self.drop = nn.Dropout(p=0.1)
         self.fc = nn.Linear(model_config.hidden_size, config["num_classes"])
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         last_hidden_state = model_out.last_hidden_state
@@ -22,8 +45,12 @@ class MeanPooling(nn.Module):
         sum_mask = torch.clamp(sum_mask, min=1e-9)
         mean_embeddings = sum_embeddings / sum_mask
 
-        out = self.drop(mean_embeddings)
-        outputs = self.fc(out)
+        if not config['multi_drop']:
+            out = self.drop(mean_embeddings)
+            outputs = self.fc(out)
+        if config['multi_drop']:
+            outputs = self.md(mean_embeddings)
+
         return outputs
 
 
@@ -32,6 +59,8 @@ class MaxPooling(nn.Module):
         super(MaxPooling, self).__init__()
         self.drop = nn.Dropout(p=0.2)
         self.fc = nn.Linear(model_config.hidden_size, config["num_classes"])
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         last_hidden_state = model_out.last_hidden_state
@@ -43,8 +72,12 @@ class MaxPooling(nn.Module):
         ] = -1e9  # Set padding tokens to large negative value
         max_embeddings = torch.max(last_hidden_state, 1)[0]
 
-        out = self.drop(max_embeddings)
-        outputs = self.fc(out)
+        if not config['multi_drop']:
+            out = self.drop(max_embeddings)
+            outputs = self.fc(out)
+        if config['multi_drop']:
+            outputs = self.md(max_embeddings)
+
         return outputs
 
 
@@ -53,6 +86,8 @@ class MeanMaxPooling(nn.Module):
         super(MeanMaxPooling, self).__init__()
         self.drop = nn.Dropout(p=0.2)
         self.fc = nn.Linear(2 * model_config.hidden_size, config["num_classes"])
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         last_hidden_state = model_out.last_hidden_state
@@ -74,8 +109,12 @@ class MeanMaxPooling(nn.Module):
 
         mean_max_embeddings = torch.cat((mean_embeddings, max_embeddings), 1)
 
-        out = self.drop(mean_max_embeddings)
-        outputs = self.fc(out)
+        if not config['multi_drop']:
+            out = self.drop(mean_max_embeddings)
+            outputs = self.fc(out)
+        if config['multi_drop']:
+            outputs = self.md(mean_max_embeddings)
+
         return outputs
 
 
@@ -84,6 +123,8 @@ class Conv1DPooling(nn.Module):
         super(Conv1DPooling, self).__init__()
         self.cnn1 = nn.Conv1d(768, 256, kernel_size=2, padding=1)
         self.cnn2 = nn.Conv1d(256, config["num_classes"], kernel_size=2, padding=1)
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         last_hidden_state = model_out.last_hidden_state
@@ -103,12 +144,18 @@ class AttentionPooling(nn.Module):
             nn.Linear(768, 512), nn.Tanh(), nn.Linear(512, 1), nn.Softmax(dim=1)
         )
         self.regressor = nn.Sequential(nn.Linear(768, 3))
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         last_hidden_state = model_out.last_hidden_state
         weights = self.attention(last_hidden_state)
         context_vector = torch.sum(weights * last_hidden_state, dim=1)
-        outputs = self.regressor(context_vector)
+
+        if not config['multi_drop']:
+            outputs = self.regressor(context_vector)
+        if config['multi_drop']:
+            outputs = self.md(context_vector)
 
         return outputs
 
@@ -117,6 +164,8 @@ class DefaultPooling(nn.Module):
     def __init__(self, model_config):
         super(DefaultPooling, self).__init__()
         self.fc = nn.Linear(model_config.hidden_size, config["num_classes"])
+        if config['multi_drop']:
+            self.md = MultiDropout(model_config=model_config)
 
     def forward(self, model_out, attention_mask):
         pooler_output = model_out.pooler_output
@@ -146,35 +195,7 @@ class FeedBackModel(nn.Module):
         out = self.pooler(out, mask)
         return out
 
-class FeedbackMultiDropout(nn.Module):
-    def __init__(self, model_name):
-        super(FeedbackMultiDropout, self).__init__()
-        self.config = AutoConfig.from_pretrained(model_name)
-        self.config.update({"output_hidden_states": True})
-        self.model = AutoModel.from_pretrained(model_name, config=self.config)
-        self.pooler = models_dict[config['pooler']](model_config=self.config)
-        
-        # Multi Dropouts
-        self.dropout1 = nn.Dropout(0.1)
-        self.dropout2 = nn.Dropout(0.2)
-        self.dropout3 = nn.Dropout(0.3)
-        self.dropout4 = nn.Dropout(0.4)
-        self.dropout5 = nn.Dropout(0.5)
-
-    def forward(self, ids, mask):
-        out = self.model(input_ids=ids, attention_mask=mask)
-        out = self.pooler(out, mask)
-
-        logits1 = self.output(self.dropout1(out))
-        logits2 = self.output(self.dropout2(out))
-        logits3 = self.output(self.dropout3(out))
-        logits4 = self.output(self.dropout4(out))
-        logits5 = self.output(self.dropout5(out))
-
-        out = (logits1 + logits2 + logits3 + logits4 + logits5) / 5
-
-        return out
 
 
 def get_model():
-    return FeedbackMultiDropout(config["model_name"])
+    return FeedBackModel(config["model_name"])
