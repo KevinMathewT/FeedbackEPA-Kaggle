1cfaf98aab1ce1fe4c293b05019278a853688674
KevinMathewT <mathewtkevin@gmail.com>
2022-08-05 19:15:27 +0530

    some changes

--- src/run.py
+++ src/run.py
@@ -48,7 +48,7 @@ def run(index):
 
         if config['use_pretrained']:
             print(f"using pretrained weights from {config['pretrained_model_weights']}")
-            model.load_state_dict(config['pretrained_model_weights'])
+            model.load_state_dict(torch.load(config['pretrained_model_weights']))
 
         model, history = get_trainer(
             model=model,
